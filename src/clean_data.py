13ee9c17053c1fb8791eef0cd176439b28b7efd7
mollysrour <srour1@stolaf.edu>
2019-06-06 16:40:04 -0500

    modular logged tested clean_data script

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,68 +1,81 @@
 import pandas as pd
 import numpy as np 
 import argparse
-import yaml
 import logging
+import yaml
 
-def create_mean_column(df, columns, name):
+def create_mean_column(df, columns, name='mean_col'):
     """Creates a column of a pandas dataframe that is a mean of values in other columns
     
     Arguments:
-        df {DataFrame} -- Pandas data frame
+        df {pd.DataFrame} -- Pandas data frame
         columns {list} -- list of columns to average values to create mean column
-        name {str} -- name of new column
+        name {str} -- name of new column (Default: {'mean_col'})
+    
+    Returns:
+        df {pd.DataFrame} - dataframe with new column added
     """
-
-    df[name] = df[columns].mean(axis=1)
+    try:
+        cols = df.loc[: ,columns] #just the columns of interest
+    except:
+        logger.error('Column names are incorrect. Re-enter column names.')
+    values = list(cols.mean(axis=1).values) #mean value across these columns of interest
+    df.loc[:, name] = values #creates new column with specified name with values as the mean values from above
     return df
 
 def omit_values(df, column, valuelist):
-    """Omits rows of dataframe that has one of a set of values in a given column 
+    """Omits rows of pandas dataframe that has one of a set of values in a given column 
     
     Arguments:
-        df {DataFrame} -- Pandas data frame
+        df {pd.DataFrame} -- Pandas data frame
         column {str} -- name of column to be evaluated
         valuelist {list} -- list of values that, if contained in the given column, means that a row should be omitted from the data frame
+    
+    Returns:
+        df {pd.DataFrame} - dataframe with rows omitted
     """
-    df = df[~df[column].isin(valuelist)]
+    df = df[~df[column].isin(valuelist)] #Only keep rows with column values NOT in valuelist
+    df = df.reset_index(drop=True) #Remove prior dataframe indexes
     return df
 
-def aggregated_category_column(df, narrowcolumn, valuedict, broadcategory):
-    """Creates an aggregated category column that has particular values dictated by values from narrowcolumn, 
-    aggregating to a broader category
-    
+def aggregated_category_column(df, narrowcolumn, valuedict, broadcategory='beer_type'):
+    """Creates an aggregated category column of a pandas dataframe that has particular values dictated by aggregating values 
+    from the categorical narrowcolumn to a broader category
     
     Arguments:
-        df {DataFrame} -- Pandas data frame
+        df {pd.DataFrame} -- Pandas data frame
         narrowcolumn {str} -- Name of narrow categorical column to be aggregated
         valuedict {dict} -- dictionary that has key: value pairs in the form of str: list. Str is 
-        the create value for the aggregated column corresponding to the list of values in the narrow column
-        broadcategory {str} --Name of the aggregated column to be created
-    """
-    for key, value in valuedict.items():
-        df.loc[df[narrowcolumn].isin(value), broadcategory] = key
+        the created value for the aggregated column corresponding to the list of values in the narrow column
+        broadcategory {str} --Name of the aggregated column to be created (Default: {'beer_type'})
+    
+    Returns:
+        df {pd.DataFrame} - dataframe with new column added
+    """ 
+    for key, value in valuedict.items(): #iterating over key/value pair in valuedict
+        df.loc[df[narrowcolumn].isin(value), broadcategory] = key #assigning key to broadcategory column for rows with values in narrow column
     return df
 
 def select_rename_features(df, oldnames, newnames):
     """Selects target features from a data frame and renames them. Returns the df with only the selected, renamed features
     
     Arguments:
-        df {DataFrame} -- Pandas data frame
+        df {pd.DataFrame} -- Pandas data frame
         oldnames {list} -- List of feature names to be included in the df (current names)
         newnames {list} -- List of names for features to be renamed
     
     Returns:
-        df {DataFrame} -- Pandas data frame with only the selected renamed features
+        df {pd.DataFrame} -- Pandas data frame with only the selected renamed features
     """
-    if range(len(oldnames)) == range(len(newnames)):
+    if range(len(oldnames)) == range(len(newnames)): #checking that names to replace is same length as old names
         for i in range(len(oldnames)):
-            df = df.rename(columns={oldnames[i]: newnames[i]})
+            df = df.rename(columns={oldnames[i]: newnames[i]}) #rename columns
     else:
-        raise ValueError('List of features to be renamed must have same length as list of new names')
-    return df[newnames]
+        logger.error('List of features to be renamed must have same length as list of new names')
+    return df[newnames] #select only renamed columns
 
 def refine_data(df, idcolname='Beer_ID', usercolname='Reviewer', n=15):
-    """Removes items from dataframe who have less than n reviews.
+    """Removes items from reviews dataframe who have less than n reviews.
     
     Arguments:
         df {pd.DataFrame} -- Pandas Data Frame 
@@ -73,40 +86,51 @@ def refine_data(df, idcolname='Beer_ID', usercolname='Reviewer', n=15):
     Returns:
         df{pd.DataFrame} -- filtered Data Frame
     """
-    df_grouped = df.groupby(idcolname).count()
-    df_grouped =  df_grouped[df_grouped[usercolname] >=n]
-    df = df[df[idcolname].isin(df_grouped.index)]
+    df_grouped = df.groupby(idcolname).count() #group by idcolname, count number of reviews
+    df_grouped =  df_grouped[df_grouped[usercolname] >=n] #only include rows with number of reviews > n
+    df = df[df[idcolname].isin(df_grouped.index)] #select rows specified from df grouped
+    df = df.reset_index(drop=True) #reset index to remove old df indexes
     return df
 
 def run_clean(args):
+    """Runs script to acquire data.
+    
+    Arguments:
+        args {argparse.Namespace} -- Script arguments, in this case, path to config file.
+    
+    Raises:
+        ValueError: "Path to yaml config file must be provided through --config" if args.config not specified
+        ValueError: "Path to CSV for input data must be provided through --input" if args.input not specified
+        ValueError: "Path to CSV for output data must be provided through --output" if args.output not specified
+    """
     if args.config is not None:
         with open(args.config, "r") as f:
 	        config = yaml.load(f)
         config = config['clean_data']
     else:
-        raise ValueError("Path to CSV for input data must be provided through --input")
+        raise ValueError("Path to yaml config file must be provided through --config")
     if args.input is not None:
         data = pd.read_csv(args.input)
     else:
-        raise ValueError("Path to yaml config file must be provided through --config")
+        raise ValueError("Path to CSV for input data must be provided through --input")
     data = create_mean_column(data, **config['create_mean_column'])
     data = omit_values(data, **config['omit_values'])
     data = aggregated_category_column(data, **config['aggregated_category_column'])
     data = select_rename_features(data, **config['select_rename_features'])
     data = refine_data(data, **config['refine_data'])
+    logger.info('Cleaned Data')
     if args.output is not None:
         data.to_csv(args.output, index=False)
     else:
         raise ValueError("Path to CSV for output data must be provided through --output")
 
-
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG, format='%(name)s - %(levelname)s - %(asctime)s - %(message)s')
     logger = logging.getLogger(__file__)
     parser = argparse.ArgumentParser(description="Add config.yml in args")
-    parser.add_argument('--config', default='config.yml', help='config.yml')
-    parser.add_argument('--input', default='../data/beer_reviews.csv', help='config.yml')
-    parser.add_argument('--output', default='../data/cleaned_beer_reviews.csv', help='config.yml')
+    parser.add_argument('--config', default='config.yml')
+    parser.add_argument('--input', default='../data/beer_reviews.csv')
+    parser.add_argument('--output', default='../data/cleaned_beer_reviews.csv')
     args = parser.parse_args()
     
     run_clean(args)
---
+++ src/test_clean_data.py
@@ -0,0 +1,95 @@
+import src.clean_data as cd
+import pandas as pd
+
+def test_create_mean_column():
+    inputs = {
+        'brewery_id': [10325, 10325, 10325, 10325, 1075],
+        'review_overall' : [1.5, 3.0, 3.0, 3.0, 4.0],
+        'review_appearance': [2.5, 3.0, 3.0, 3.5, 4.0]}
+    the_input = pd.DataFrame(inputs)
+    columns = ['review_overall', 'review_appearance']
+    name = 'mean_review'
+    answers = {
+        'brewery_id': [10325, 10325, 10325, 10325, 1075],
+        'review_overall' : [1.5, 3.0, 3.0, 3.0, 4.0],
+        'review_appearance': [2.5, 3.0, 3.0, 3.5, 4.0], 
+        'mean_review': [2.00, 3.00, 3.00, 3.25, 4.00]}
+    output = pd.DataFrame(answers)
+    assert isinstance(cd.create_mean_column(the_input, columns, name), pd.DataFrame)
+    assert output.equals(cd.create_mean_column(the_input, columns, name))
+
+def test_omit_values():
+    inputs = {
+        'brewery_id': [10325, 10325, 10325, 10325, 1075],
+        'review_overall' : [1.5, 3.0, 3.0, 3.0, 4.0],
+        'review_appearance': [2.5, 3.0, 3.0, 3.5, 4.0],
+        'beer_style': ['Hefeweizen', 'English Strong Ale', 'Foreign / Export Stout', 'German Pilsener', 'American Double / Imperial IPA']}
+    the_input = pd.DataFrame(inputs)
+    column = 'beer_style'
+    valuelist = ['Hefeweizen', 'English Strong Ale']
+    answers = {
+        'brewery_id': [10325, 10325, 1075],
+        'review_overall' : [3.0, 3.0, 4.0],
+        'review_appearance': [3.0, 3.5, 4.0], 
+        'beer_style': ['Foreign / Export Stout', 'German Pilsener', 'American Double / Imperial IPA']}
+    output = pd.DataFrame(answers)
+    assert isinstance(cd.omit_values(the_input, column, valuelist), pd.DataFrame)
+    assert cd.omit_values(the_input, column, valuelist).shape[0] == 3
+    assert output.equals(cd.omit_values(the_input, column, valuelist))
+
+def test_aggregated_category_column():
+    inputs = {
+        'brewery_id': [10325, 10325, 10325, 10325, 1075],
+        'review_overall' : [1.5, 3.0, 3.0, 3.0, 4.0],
+        'review_appearance': [2.5, 3.0, 3.0, 3.5, 4.0],
+        'beer_style': ['Hefeweizen', 'English Strong Ale', 'Foreign / Export Stout', 'German Pilsener', 'American Double / Imperial IPA']}
+    the_input = pd.DataFrame(inputs)
+    narrowcolumn = 'beer_style'
+    valuedict = {'BigBeer1': ['Hefeweizen','English Strong Ale', 'Foreign / Export Stout'], 'BigBeer2': ['German Pilsener', 'American Double / Imperial IPA']}  
+    broadcategory = 'beer_type'
+    answers = {
+        'brewery_id': [10325, 10325, 10325, 10325, 1075],
+        'review_overall' : [1.5, 3.0, 3.0, 3.0, 4.0],
+        'review_appearance': [2.5, 3.0, 3.0, 3.5, 4.0],
+        'beer_style': ['Hefeweizen', 'English Strong Ale', 'Foreign / Export Stout', 'German Pilsener', 'American Double / Imperial IPA'],
+        'beer_type': ['BigBeer1', 'BigBeer1', 'BigBeer1', 'BigBeer2', 'BigBeer2']}
+    output = pd.DataFrame(answers)
+    assert isinstance(cd.aggregated_category_column(the_input, narrowcolumn, valuedict, broadcategory), pd.DataFrame)
+    assert output.equals(cd.aggregated_category_column(the_input, narrowcolumn, valuedict, broadcategory))
+
+def test_select_rename_features():
+    inputs = {
+        'brewery_id': [10325, 10325, 10325, 10325, 1075],
+        'review_overall' : [1.5, 3.0, 3.0, 3.0, 4.0],
+        'review_appearance': [2.5, 3.0, 3.0, 3.5, 4.0],
+        'beer_style': ['Hefeweizen', 'English Strong Ale', 'Foreign / Export Stout', 'German Pilsener', 'American Double / Imperial IPA']}
+    the_input = pd.DataFrame(inputs)
+    oldnames = ['brewery_id', 'review_overall', 'beer_style']
+    newnames = ['Brewery', 'Review Overall', 'Beer Style']
+    answers = {
+        'Brewery': [10325, 10325, 10325, 10325, 1075],
+        'Review Overall' : [1.5, 3.0, 3.0, 3.0, 4.0],
+        'Beer Style': ['Hefeweizen', 'English Strong Ale', 'Foreign / Export Stout', 'German Pilsener', 'American Double / Imperial IPA']}
+    output = pd.DataFrame(answers)
+    assert cd.select_rename_features(the_input, oldnames, newnames).shape[1] == 3
+    assert isinstance(cd.select_rename_features(the_input, oldnames, newnames), pd.DataFrame)
+    assert output.equals(cd.select_rename_features(the_input, oldnames, newnames))
+
+def test_refine_data():
+    inputs = {
+        'Beer_ID': ['BeerA', 'BeerA', 'BeerA', 'BeerA', 'ReviewerB'],
+        'review_overall' : [1.5, 3.0, 3.0, 3.0, 4.0],
+        'Reviewer': ['ReviewerA', 'ReviewerB', 'ReviewerC', 'ReviewerD', 'ReviewerA']}
+    the_input = pd.DataFrame(inputs)
+    idcolname='Beer_ID'
+    usercolname='Reviewer'
+    n=3
+    print(cd.refine_data(the_input, idcolname, usercolname, n))
+    answers = {
+        'Beer_ID': ['BeerA', 'BeerA', 'BeerA', 'BeerA'],
+        'review_overall' : [1.5, 3.0, 3.0, 3.0],
+        'Reviewer': ['ReviewerA', 'ReviewerB', 'ReviewerC', 'ReviewerD']}
+    output = pd.DataFrame(answers)
+    assert cd.refine_data(the_input, idcolname, usercolname, n).shape[0] == 4
+    assert isinstance(cd.refine_data(the_input, idcolname, usercolname, n), pd.DataFrame)
+    assert output.equals(cd.refine_data(the_input, idcolname, usercolname, n))
