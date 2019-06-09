ce02629f355621bd2ea072143800cce5e7db7ec3
mollysrour <srour1@stolaf.edu>
2019-06-09 14:51:17 -0500

    ready for QA

--- flask_config.py
+++ flask_config.py
@@ -1,8 +1,21 @@
+import os
 DEBUG = True
-PORT = 3000
 APP_NAME = "beerapp"
-SQLALCHEMY_DATABASE_URI = 'sqlite:///data/beers.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-HOST = "8000"
+
+
 SQLALCHEMY_ECHO = True  # If true, SQL for queries made will be printed
 MAX_ROWS_SHOW = 100
+
+conn_type = "mysql+pymysql"  
+user = os.environ.get('MYSQL_USER')
+password = os.environ.get('MYSQL_PASSWORD')
+port = os.environ.get('MYSQL_PORT')
+DATABASE_NAME = 'msia423' 
+host = os.environ.get('MYSQL_HOST')
+if host is None:
+    host = "8000"
+if user is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///data/beers.db' 
+else:
+    SQLALCHEMY_DATABASE_URI = "{}://{}:{}@{}:{}/{}".\
+    format(conn_type, user, password, host, port, DATABASE_NAME)
